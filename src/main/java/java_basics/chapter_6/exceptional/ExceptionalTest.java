801f4176dfb2903eb50b64d2ebb4d87b31bd9e16
adrianromanski <adrianromanski@icloud.com>
2020-08-12 11:01:57 +0200

    Added Chapter 6  - Java Core Fundamentals

---
+++ src/main/java/java_basics/chapter_6/exceptional/ExceptionalTest.java
@@ -0,0 +1,40 @@
+package java_basics.chapter_6.exceptional;
+
+import java.util.Date;
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class ExceptionalTest {
+
+    public static void main(String[] args) {
+
+        int i  = 0;
+        int ntry = 100000;
+        Stack<String> s = new Stack<>();
+        long s1;
+        long s2;
+
+        System.out.println("Testing for empty stack");
+        s1 = new Date().getTime();
+        for(i = 0; i <= ntry; i ++) {
+            if(!s.empty()) {
+                s.pop();
+            }
+        }
+        s2 = new Date().getTime();
+        System.out.println((s2 - s1) + " milliseconds");
+
+        System.out.println("Catching EmptyStackException");
+
+        s1 = new Date().getTime();
+        for(i = 0; i <= ntry; i++) {
+            try {
+                s.pop();
+            }
+            catch (EmptyStackException e) {
+            }
+        }
+        s2 = new Date().getTime();
+        System.out.println((s2 - s1) + " milliseconds");
+    }
+}

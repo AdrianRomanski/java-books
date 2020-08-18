186609ffc69531cc9c88924dd0f538ee55bd9873
adrianromanski <adrianromanski@icloud.com>
2020-08-18 12:31:07 +0200

    Added Chapter 9 - Java Core Fundamentals

---
+++ src/main/java/java_basics/chapter_9/listFonts/ListFonts.java
@@ -0,0 +1,14 @@
+package java_basics.chapter_9.listFonts;
+
+import java.awt.*;
+
+public class ListFonts {
+    public static void main(String[] args) {
+
+        String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getAvailableFontFamilyNames();
+
+        for (String fontName : fontNames)
+            System.out.println(fontName);
+    }
+}
